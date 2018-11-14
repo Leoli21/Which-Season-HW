a1bd94630c0d38fab4b7b9f1e709d8b9bdb8285b
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2018-11-14 08:39:28 -0500

    Add files via upload

---
+++ WhichSeason.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class WhichSeason 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please input numerical value of your birth month ");;
+		int birthMonth = input.nextInt();
+		if (birthMonth >= 1 || birthMonth <=12);
+		{
+			if (birthMonth >= 1 || birthMonth <= )
+			{
+				System.out.println("You were born in the the winter.");
+			}
+				
+		
+		
+		}
+		else
+		{
+			System.out.println("Your input is invalid. Enter a value from 1 to 12.");
+		}
+	}
+
+}
